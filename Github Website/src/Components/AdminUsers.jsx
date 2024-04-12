73cfc34fcaf646a61c05a705b73dafbae1198767
Tman360A <110684192+Tman360A@users.noreply.github.com>
2024-04-12 13:47:49 +0000

    added user searching

--- Github Website/src/Components/AdminUsers.jsx
+++ Github Website/src/Components/AdminUsers.jsx
@@ -3,40 +3,51 @@ import React, { useState, useEffect } from "react";
 import AdminUserTab from "./AdminUserTab";
 
 function AdminUsers() {
-    const [data, setData] = useState([]);
+  const [data, setData] = useState([]);
+  const [searchQuery, setSearchQuery] = useState("");
 
-    useEffect(() => {
-        fetchData();
-    }, []);
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-    async function fetchData() {
-        try {
-            const httpRequest = "https://boss-grouper-explicitly.ngrok-free.app/api/accounts";
-            const account = await fetch(httpRequest, {
-                method: "GET",
-                headers: { "ngrok-skip-browser-warning": "true"}
-            });
-
-            if (!account.ok) {
-                throw new Error("Failed to fetch user data");
-            }
-
-            const userData = await account.json();
-            console.log("Fetched data:", userData); // Log fetched data
-            setData(userData);
-
-        } catch (error) {
-            console.error("Error fetching data:", error);
+  async function fetchData() {
+    try {
+      const httpRequest = "https://boss-grouper-explicitly.ngrok-free.app/api/accounts";
+      const accounts = await fetch(httpRequest, {
+        method: "GET",
+        headers: {
+          "ngrok-skip-browser-warning": "true"
         }
-    }
+      });
 
-    return(
-        <div className="UserList">
-            {data.map((user) => (
-                <AdminUserTab id={user.id} userName={user.userName} password={user.password} />
-            ))}
-        </div>
-    );
+      if (!accounts.ok) {
+        throw new Error("Failed to fetch user data");
+      }
+
+      const userData = await accounts.json();
+      console.log("Fetched data:", userData); // Log fetched data
+      setData(userData);
+
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  }
+
+  const handleSearchChange = (event) => {
+    const query = event.target.value.toLowerCase();
+    setSearchQuery(query);
+  };
+
+  const filteredData = data.filter((user) => user.userName.toLowerCase().includes(searchQuery));
+
+  return (
+    <div className="UserList">
+      <input type="text" value={searchQuery} onChange={handleSearchChange} placeholder="User Name..." />
+      {filteredData.map((user) => (
+        <AdminUserTab key={user.id} id={user.id} userName={user.userName} password={user.password} />
+      ))}
+    </div>
+  );
 }
 
 export default AdminUsers;
