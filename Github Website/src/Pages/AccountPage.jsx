db947347b91b4227509746cd65482c7a812929cf
Tman360 <Tman3601@outlook.com>
2024-04-16 19:16:20 -0500

    added some changes

--- Github Website/src/Pages/AccountPage.jsx
+++ Github Website/src/Pages/AccountPage.jsx
@@ -1,56 +1,16 @@
-import "../Css/Pages/AccountPage.css"
+import "../Css/Pages/AccountPage.css";
 import { useEffect, useState } from 'react';
-import { useParams, } from 'react-router-dom'
+import { useParams } from 'react-router-dom';
+import GlitchText from "../Util/GlitchText"
 
 function AccountPage() {
-    const {userName} = useParams();
+    const { userName } = useParams();
     const [userEmail, setUserEmail] = useState("");
     const [userPassword, setUserPassword] = useState("");
-    const [text, setText] = useState("");
-    const [finalText, setFinalText] = useState("Tman360");
-    const [finalTextSlice, setfinalTextSlice] = useState(0);
-    const [textSlice, setTextSlice] = useState("");
 
     useEffect(() => {
-        const periodic = setInterval(() => {
-            generateRandomText();
-        }, 50);
-
-        const incrementPeriodic = setInterval(() => {
-            increment();
-        }, 200);
-
         getAccountData();
-
-        // Cleanup function to clear the interval when the component unmounts
-        return () => {
-            clearInterval(periodic);
-            clearInterval(incrementPeriodic);
-        };
-    }, []);
-
-    useEffect(() => {
-        if (finalText.slice(0, finalTextSlice) === finalText) {
-            setTextSlice(1);
-        }
-    }, [finalText, finalTextSlice]);
-
-    const generateRandomText = () => {
-        const randomText = Array.from({ length: 1 }, () =>
-          String.fromCharCode(Math.floor(Math.random() * 26) + 97)
-        ).join("");
-
-        setText(randomText);
-        setTextSlice(textSlice + randomText);
-    };
-
-    function increment() {
-        if (finalText.length > finalTextSlice) {
-            setfinalTextSlice(prevSlice => prevSlice + 1);
-        } else {
-            setText(1)
-        };
-    }
+    }, []) 
 
     const getAccountData = async () => {
         try {
@@ -76,16 +36,13 @@ function AccountPage() {
         }
     }
 
-    return(
+    return (
         <div className="AccountPage">
-            <p>{userName}</p>
+            <p>{GlitchText(userName)}</p>
             <p>{userEmail}</p>
             <p>{userPassword}</p>
-            <p>{finalText.slice(0, finalTextSlice) + textSlice}</p>
-            <p>{finalTextSlice}</p>
-            <button onClick={increment}>Increment</button>
         </div>
     );
 }
 
-export default AccountPage
+export default AccountPage;
---
+++ Github Website/src/Util/GlitchText.jsx
@@ -0,0 +1,44 @@
+import { useEffect, useState } from "react";
+
+function GlitchText(Word) {
+
+    const [glitch, setGlitch] = useState("");
+    const [wordLength, setwordLength] = useState(0);
+    const [glitchLength, setGlitchLength] = useState(1);
+
+    useEffect(() => {
+        const glitchInterval = setInterval(generateRandomText, 10);
+        const addLetterInterval = setInterval(addLetter, 300);
+
+        return () => {
+            clearInterval(glitchInterval);
+            clearInterval(addLetterInterval);
+        };
+    }, []);
+
+    useEffect(() => {
+
+        if (Word.slice(0, wordLength) === Word) {
+            setGlitchLength(0)
+        }
+    }, [Word, wordLength])
+
+    const generateRandomText = () => {
+        const randomText = Array.from({ length: 1 }, () =>
+            String.fromCharCode(Math.floor(Math.random() * 26) + 97)
+        ).join("");
+
+        setGlitch(randomText);
+    };
+
+    const addLetter = () => {
+        if (Word.length > wordLength) {
+            setwordLength(length => length + 1);
+        }         
+    }
+
+    return Word.slice(0, wordLength) + glitch.slice(0, glitchLength);
+
+}
+
+export default GlitchText
